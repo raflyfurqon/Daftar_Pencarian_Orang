2d674ae94f588b0631be370fad854f1fe3304e72
Rafly Furqon Musharaf <148001472+raflyfurqon@users.noreply.github.com>
2024-06-02 20:49:36 +0700

    Source Kode Awal

---
+++ HashTableJava.java
@@ -0,0 +1,24 @@
+package hashtable.java;
+import java.util.*;
+import java.util.Hashtable;
+
+public class HashTableJava {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> pembunuhMap = new HashMap<>();
+        Set<String> korbanSet = new HashSet<>();
+
+        // Membaca input
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.trim().isEmpty()) {
+                break;
+            }
+            String[] names = line.split(" ");
+            String pembunuh = names[0];
+            String korban = names[1];
+
+            // Menambahkan pembunuh dan korban ke dalam struktur data
+            pembunuhMap.put(pembunuh, pembunuhMap.getOrDefault(pembunuh, 0) + 1);
+            korbanSet.add(korban);
+        }

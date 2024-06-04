e9b6b06f3875eb7e08b2ec414c42bd157d7e6e2a
Ripan <ripanrediansyah@gmail.com>
2024-06-04 20:11:40 +0700

    Mneghapus coment

--- HashTableJava.java
+++ HashTableJava.java
@@ -7,7 +7,6 @@ public class HashTableJava {
         Map<String, Integer> pembunuhMap = new HashMap<>();
         Set<String> korbanSet = new HashSet<>();
 
-        // Membaca input
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.trim().isEmpty()) {
@@ -17,21 +16,17 @@ public class HashTableJava {
             String pembunuh = names[0];
             String korban = names[1];
 
-            // Menambahkan pembunuh dan korban ke dalam struktur data
             pembunuhMap.put(pembunuh, pembunuhMap.getOrDefault(pembunuh, 0) + 1);
             korbanSet.add(korban);
         }
         scanner.close();
 
-        // Menghapus pembunuh yang juga merupakan korban
         for (String korban : korbanSet) {
             pembunuhMap.remove(korban);
         }
-        // Mengurutkan nama pembunuh secara abjad
         List<String> daftarPembunuh = new ArrayList<>(pembunuhMap.keySet());
         Collections.sort(daftarPembunuh);
 
-        // Menampilkan output
         System.out.println("DAFTAR PENCARIAN ORANG KASUS PEMBUNUHAN");
         for (String pembunuh : daftarPembunuh) {
             System.out.println(pembunuh + " " + pembunuhMap.get(pembunuh));
